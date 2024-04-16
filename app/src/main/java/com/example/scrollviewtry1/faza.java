6729b89306907a4552f686f65b79fa14068d293e
mariemgharsallah <maryemgharssalla@gmail.com>
2024-04-16 18:22:12 +0100

    first

---
+++ app/src/main/java/com/example/scrollviewtry1/faza.java
@@ -0,0 +1,14 @@
+package com.example.scrollviewtry1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class faza extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faza);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "scroll view try1"
+include(":app")
+ 
